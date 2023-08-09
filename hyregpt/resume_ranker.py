935f355eecc12a02908d20f870922f88a38a61a8
thedatamonk <rohilpal9763@gmail.com>
2023-08-09 08:26:41 +0530

    added resume ranker code

--- hyregpt/resume_ranker.py
+++ hyregpt/resume_ranker.py
@@ -1,26 +1,53 @@
-import openai
+import html2text
+import textract
+import os
+
+def html_to_text(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+
+    h = html2text.HTML2Text()
+    h.ignore_links = False
+    h.body_width = 0
+
+    markdown_content = h.handle(html_content)
+
+    return markdown_content
 
 
-RESUME_RANKER_TEMPLATE = """
+def pdf_to_text(file_path):
+    return textract.process(file_path)
 
 
-"""
+def compute_similarity_score(job_description_file_path, candidate_resumes):
 
-def rank_resumes(resumes):
-
-    resume_ranker_prompt = RESUME_RANKER_TEMPLATE.format(resumes=resumes)
-
-    # LLM call
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-            {"role": "user", "content": resume_ranker_prompt}
-        ]
-    )
-
-    ranked_resumes = response.choices[0].message.content
-
-    # processed ranked_resumes and convert it into a list and then store this info somewhere
-
-    return ranked_resumes
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.metrics.pairwise import cosine_similarity
     
+
+    # create vocab of job description
+    job_description = html_to_text(job_description_file_path)
+    vectorizer = CountVectorizer().fit([job_description])
+
+    jd_vector = vectorizer.transform([job_description])
+
+    scores = {}
+    for resume_path in candidate_resumes:
+
+        filename = os.path.basename(resume_path)
+
+        resume = pdf_to_text(resume_path)
+    
+        # create count matrix for each document; each row is a doc and each column indicates a word in the vocab
+        # TODO: this is a very naive way to convert words to numbers and will not capture any semantics. 
+        # We should definitely use word embeddings to capture semantics
+
+        resume_vector = vectorizer.transform([resume])
+
+
+        similarity_score = cosine_similarity(jd_vector, resume_vector)[0][0] # for countvectorizer, the similarity score is in [0, 1] range
+        similarity_score = round(similarity_score * 100, 2) # [0, 100]
+        scores[filename] = similarity_score
+    
+    
+    return scores    
--- pages/Rank_Resumes.py
+++ pages/Rank_Resumes.py
@@ -1,20 +1,74 @@
 import streamlit as st
 import os
+from hyregpt.resume_ranker import compute_similarity_score
+import pandas as pd
 
-def process_selected_files(selected_files):
-    for file in selected_files:
-        st.write(f"You selected {file}")
 
-# Display files in the given directory
-def list_files_in_directory(dir_path="."):
+st.title("Resume ranking")
+
+
+def score_resumes():
+
+    if 'selected_jd' not in st.session_state.keys():
+        st.error("No job description file found. Please upload a valid job description file from your local file storage.")
+    
+    if not st.session_state['selected_jd'].endswith('.html'):
+        st.error("Job description file should be of HTML type.")
+
+    if 'selected_resumes' not in st.session_state.keys():
+        st.error("No resumes are selected. Please select atleast one valid resume.")
+
+    if not all(file_path.endswith(".pdf") for file_path in st.session_state['selected_resumes']):
+        st.error("One or more resume files are not of pdf type.")
+
+
+    resume_scores = compute_similarity_score(
+        job_description_file_path=st.session_state['selected_jd'],
+        candidate_resumes=st.session_state['selected_resumes']
+    )
+
+
+    resume_scores_table = pd.DataFrame({
+        "Resume": list(resume_scores.keys()),
+        "Score": list(resume_scores.values())
+    })
+
+    st.table(resume_scores_table)
+    
+
+def list_resumes(dir_path="tests/resumes"):
     try:
-        files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
-        selected_files = st.multiselect('Select one or more files for processing:', files)
-        if st.button('Process Selected Files'):
-            process_selected_files(selected_files)
+        all_resumes = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f)) and f.endswith('.pdf')]
+        selected_resumes = st.multiselect('Select one or more resumes for processing:', all_resumes)
+
+        st.session_state['selected_resumes'] = [os.path.join(st.secrets['filepaths']['resume_directory'], f) for f in selected_resumes]
+
+        if st.button('Analyse and score resumes'):
+            score_resumes()
     except Exception as e:
-        st.write("An error occurred:", e)
+        st.write("Error occurred while selecting resumes: ", e)
 
-dir_path = "tests/job_descriptions/html"
 
-list_files_in_directory(dir_path)
+def select_job_description(dir_path="tests/job_descriptions/html"):
+    try:
+        all_jds = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f)) and f.endswith('.html')]
+
+        placeholder_option = "Select a job description file..."
+        all_jds =  [placeholder_option] + all_jds
+
+        selected_jd = st.selectbox('Select a job description from your local storage:', all_jds)
+
+        if selected_jd != placeholder_option:
+            st.session_state['selected_jd'] = os.path.join(st.secrets['filepaths']['job_description_directory'], selected_jd)
+
+    except Exception as e:
+        st.write("Error occurred while selecting job description file: ", e)
+
+
+st.markdown("""## Job Description""")
+select_job_description()
+
+
+st.markdown("""## Resumes""")
+list_resumes()
+
