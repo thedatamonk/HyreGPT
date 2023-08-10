a300b1302c7eefd9a15e591024a4a879933179e7
thedatamonk <rohilpal9763@gmail.com>
2023-08-10 17:40:54 +0530

    updated chat interface by adding avatars, and logic to display previous messages

--- pages/Screening_Round.py
+++ pages/Screening_Round.py
@@ -1,4 +1,17 @@
 import streamlit as st
+from hyregpt.screening_round import conversation_chain
+from ui.screening_interview_ui import message_func
+
+
+def append_chat_history(question, answer):
+    st.session_state["history"].append((question, answer))
+
+def append_message(content, role="recruiter", display=False):
+    message = {"role": role, "content": content}
+    message_func(content, False, display)
+    st.session_state.messages.append(message)
+    append_chat_history(st.session_state.messages[-2]["content"], content)
+
 
 st.title("Screening interview")
 st.sidebar.header("Candidate details")
@@ -26,12 +39,50 @@ if start_interview_button:
     st.session_state.candidate_name = candidate_name
     st.session_state.candidate_resume = candidate_resume
     st.session_state.job_desc = job_desc
+    st.session_state.interview_started = True
 
-    st.markdown(f"Hi {st.session_state.candidate_name}!")
-    st.chat_input()
 
+
+
+if st.session_state.interview_started:
+    INITIAL_MESSAGE = [
+        {"role": "recruiter", "content": f"""Hi {st.session_state.candidate_name}! Welcome to AlphaMind's screening interview!\n Are you ready for the interview?"""}
+    ]
+
+    if "messages" not in st.session_state.keys():
+        st.session_state['messages'] = INITIAL_MESSAGE
+
+    if "history" not in st.session_state.keys():
+        st.session_state["history"] = []
+
+
+    if user_input := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": user_input})
+
+    for message in st.session_state.messages:
+        message_func(
+            message["content"],
+            True if message["role"] == "user" else False,
+            True if message["role"] == "data" else False,
+        )
+    
+
+    if st.session_state.messages[-1]["role"] != "recruiter":
+        content = st.session_state.messages[-1]["content"]
+        if isinstance(content, str):
+            result = conversation_chain({"input": content})["response"]
+
+            append_message(result)
+    
 
 if st.sidebar.button("Reset"):
     del st.session_state.candidate_details_submitted
     del st.session_state.interview_started
     del st.session_state.candidate_name
+    del st.session_state.candidate_resume
+    del st.session_state.job_desc
+    del st.session_state.messages
+    del st.session_state.history
+
+    st.session_state["messages"] = INITIAL_MESSAGE
+    st.session_state["history"] = []
---
+++ ui/screening_interview_ui.py
@@ -0,0 +1,81 @@
+import pandas
+import streamlit as st
+import re
+import html
+
+
+def format_message(text):
+    """
+    This function is used to format the messages in the chatbot UI.
+
+    Parameters:
+    text (str): The text to be formatted.
+    """
+    text_blocks = re.split(r"```[\s\S]*?```", text)
+    code_blocks = re.findall(r"```([\s\S]*?)```", text)
+
+    text_blocks = [html.escape(block) for block in text_blocks]
+
+    formatted_text = ""
+    for i in range(len(text_blocks)):
+        formatted_text += text_blocks[i].replace("\n", "<br>")
+        if i < len(code_blocks):
+            formatted_text += f'<pre style="white-space: pre-wrap; word-wrap: break-word;"><code>{html.escape(code_blocks[i])}</code></pre>'
+
+    return formatted_text
+
+
+def message_func(text, is_user=False, is_df=False):
+    """
+    This function is used to display the messages in the chatbot UI.
+
+    Parameters:
+    text (str): The text to be displayed.
+    is_user (bool): Whether the message is from the user or not.
+    is_df (bool): Whether the message is a dataframe or not.
+    """
+    if is_user:
+        avatar_url = "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortFlat&accessoriesType=Prescription01&hairColor=Auburn&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=PastelBlue&eyeType=Squint&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Tanned"
+        message_alignment = "flex-end"
+        message_bg_color = "linear-gradient(135deg, #00B2FF 0%, #006AFF 100%)"
+        avatar_class = "user-avatar"
+        st.write(
+            f"""
+                <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
+                    <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px;">
+                        {text} \n </div>
+                    <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
+                </div>
+                """,
+            unsafe_allow_html=True,
+        )
+    else:
+        avatar_url = "https://avataaars.io/?avatarStyle=Transparent&topType=WinterHat2&accessoriesType=Kurt&hatColor=Blue01&facialHairType=MoustacheMagnum&facialHairColor=Blonde&clotheType=Overall&clotheColor=Gray01&eyeType=WinkWacky&eyebrowType=SadConcernedNatural&mouthType=Sad&skinColor=Light"
+        message_alignment = "flex-start"
+        message_bg_color = "#71797E"
+        avatar_class = "bot-avatar"
+
+        if is_df:
+            st.write(
+                f"""
+                    <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
+                        <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
+                    </div>
+                    """,
+                unsafe_allow_html=True,
+            )
+            st.write(text)
+            return
+        else:
+            text = format_message(text)
+
+        st.write(
+            f"""
+                <div style="display: flex; align-items: center; margin-bottom: 10px; justify-content: {message_alignment};">
+                    <img src="{avatar_url}" class="{avatar_class}" alt="avatar" style="width: 50px; height: 50px;" />
+                    <div style="background: {message_bg_color}; color: white; border-radius: 20px; padding: 10px; margin-right: 5px; max-width: 75%; font-size: 14px;">
+                        {text} \n </div>
+                </div>
+                """,
+            unsafe_allow_html=True,
+        )
